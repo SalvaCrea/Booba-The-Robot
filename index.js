4878cb93ef7e2556613f3932d30fdb5e93ad0c54
Cardona <hebustha@gmail.com>
2018-05-09 22:59:10 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+var Gpio = require('onoff').Gpio;
+var led = new Gpio(17, 'out');
+
+app.get('/', function (req, res) {
+      res.send('Hello World!');
+});
+
+app.get('/on-led', function (req, res) {
+      led.writeSync(value);
+});
+
+app.listen(3000, function () {
+      console.log('Example app listening on port 3000!');
+});
