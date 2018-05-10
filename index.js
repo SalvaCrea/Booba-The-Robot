457ade49b5c87d075eccc06c106676d9385691f0
Cardona <hebustha@gmail.com>
2018-05-10 14:37:56 +0200

    ++

--- index.js
+++ index.js
@@ -2,23 +2,38 @@ var express = require('express');
 var app     = express();
 var exec    = require('child_process').exec;
 
-// var Gpio    = require('onoff').Gpio;
-// var led     = new Gpio(17, 'out');
+var Gpio    = require('onoff').Gpio;
+var led     = new Gpio(17, 'out');
 
-var currentPath = process.cwd();
+function blinkLED() { //function to start blinking
+      if (led.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+            led.writeSync(1); //set pin state to 1 (turn led on)
+      } else {
+            led.writeSync(0); //set pin state to 0 (turn led off)
+      }
+}
+
+function endBlink() { //function to stop blinking
+      clearInterval(blinkInterval); // Stop blink intervals
+      led.writeSync(0); // Turn led off
+      led.unexport(); // Unexport GPIO to free resources
+}
 
 app.get('/', function (req, res) {
       res.send('Hello World!');
 });
 
 app.get('/on-led', function (req, res) {
-      led.writeSync(1);
+      blinkLED();
+});
+
+app.get('/off-led', function (req, res) {
+      endBlink();
 });
 
 app.get('/update', function (req, res) {
     exec('make update',function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
+        res.send('Update Do It');
     } )
 });
 
